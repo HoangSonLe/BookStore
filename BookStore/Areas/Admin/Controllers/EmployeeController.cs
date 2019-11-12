4b2bd743e5e2bae9441641c84f4378426ac8cf34
Quoc Ky <dqky.hcmup@gmail.com>
2019-11-12 07:29:57 +0700

    Created function Add Detail Edit Delete Employee Add file employee.js Edit file _MyMenu.cshtml

--- BookStore/Areas/Admin/Controllers/EmployeeController.cs
+++ BookStore/Areas/Admin/Controllers/EmployeeController.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using BookStore.Helpers;
+using System;
 
 namespace BookStore.Areas.Admin.Controllers
 {
@@ -12,9 +15,130 @@ namespace BookStore.Areas.Admin.Controllers
 
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly MyDBContext _context;
+
+        public EmployeeController(MyDBContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _context.Employee.ToListAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            var info = HttpContext.Session.GetObject<Employee>("Employee");
+            Employee emp = new Employee();
+            ViewData["RoleId"] = new SelectList(_context.Roles, "RoleId", "RoleName", info.Role);
+            var managers = _context.Employee.Where(p => p.Role < info.Role).ToList();
+
+            ViewBag.Managers = managers;
+            return View(emp);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Employee employee)
+        {
+            bool check = _context.Employee.Any(e => e.EmployeeId == employee.EmployeeId);
+            employee.CreatedDate = DateTime.Now;
+            if (!check)
+            {
+                _context.Employee.Add(employee);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                return BadRequest();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var employee = await _context.Employee
+                .Include(e => e.RoleNavigation)
+                .Include(e => e.Manager)
+                .FirstOrDefaultAsync(e => e.EmployeeId == id);
+
+            if (employee == null)
+            {
+                return BadRequest();
+            }
+
+            return PartialView(employee);
+        }
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee == null)
+            {
+                return BadRequest();
+            }
+
+            var info = HttpContext.Session.GetObject<Employee>("Employee");
+            ViewData["RoleId"] = new SelectList(_context.Roles, "RoleId", "RoleName", 1);
+            var managers = _context.Employee.Where(p => p.Role < 1).ToList();
+
+            ViewBag.Managers = managers;
+            return View(employee);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Employee employee)
+        {
+            var emp = await _context.Employee.AsNoTracking().SingleOrDefaultAsync(e=>e.EmployeeId == employee.EmployeeId);
+
+            if (employee.Password != emp.Password)
+            {
+                employee.Password = MyHashTool.GetMd5Hash(employee.Password);
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            var info = HttpContext.Session.GetObject<Employee>("Employee");
+            ViewData["RoleId"] = new SelectList(_context.Roles, "RoleId", "RoleName", info.Role);
+            var managers = _context.Employee.Where(p => p.Role < info.Role).ToList();
+
+            ViewBag.Managers = managers;
+            return View(employee);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var employee = await _context.Employee.AsNoTracking().SingleOrDefaultAsync(e => e.EmployeeId == id);
+            var info = HttpContext.Session.GetObject<Employee>("Employee");
+            if (employee.Role <= info.Role)
+            {
+                return BadRequest();
+            }
+            _context.Employee.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ BookStore/wwwroot/Admin/js/Employee.js
@@ -0,0 +1,135 @@
+﻿var table = $('#tableEmployees').DataTable();
+
+$("#formAdd").on('submit', function (e) {
+    e.preventDefault();
+    var employee = $("#formAdd").serialize();
+    $.ajax({
+        url: "/Admin/Employee/Add",
+        type: "POST",
+        data: employee,
+        success: function () {
+            Swal.fire({
+                icon: 'success',
+                title: 'Thành công!',
+                text: 'Thêm thành công!',
+                showConfirmButton: false,
+                timer: 1000
+            });
+        },
+        error: function () {
+            Swal.fire({
+                icon: 'fail',
+                title: 'Thất bại!',
+                text: 'Thêm không thành công!',
+                showConfirmButton: false,
+                timer: 1000
+            });
+        }
+    });
+
+});
+
+$(".btnDetail").click(function () {
+    var id = $(this).data("id");
+    $.ajax({
+        url: "/Admin/Employee/Details",
+        type: "GET",
+        data: {
+            id: id
+        },
+        success: function (data) {
+            $("#modalBody").html("");
+            $("#modalBody").html(data);
+        }
+    });
+});
+
+$(".btnEdit").click(function () {
+    var id = $(this).data("id");
+    $.ajax({
+        url: "/Admin/Employee/Edit",
+        type: "GET",
+        data: {
+            id: id
+        },
+        success: function (data) {
+            $(".box").html("");
+            $(".box").html(data);
+        }
+    });
+});
+
+$("#formEdit").on('submit', function (e) {
+    e.preventDefault();
+    var employee = $("#formEdit").serialize();
+    $.ajax({
+        url: "/Admin/Employee/Edit",
+        type: "POST",
+        data: employee,
+        success: function () {
+            Swal.fire({
+                icon: 'success',
+                title: 'Thành công!',
+                text: 'Sửa thành công!',
+                showConfirmButton: false,
+                timer: 1000
+            });
+        },
+        error: function () {
+            Swal.fire({
+                icon: 'error',
+                title: 'Thất bại!',
+                text: 'Sửa không thành công!',
+                showConfirmButton: false,
+                timer: 1000
+            });
+        }
+    });
+});
+
+$(".btnDelete").click(function () {
+    var id = $(this).data("id");
+    var item = $(this).parents('tr');
+    Swal.fire({
+        html: '<h2>Bạn có chắn xóa thành viên có mã <b>' + id + "</b>?</h2>",
+        text: "Bạn không thể hoàn tác!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Đúng, xóa.!'
+    }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                url: "/Admin/Employee/Delete",
+                type: "POST",
+                data: {
+                    id: id
+                },
+                success: function () {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Thành công!',
+                        text: 'Xóa thành công!',
+                        showConfirmButton: false,
+                        timer: 1000
+                    });
+                    table
+                         .row(item)
+                         .remove()
+                         .draw();
+                },
+                error: function () {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Lỗi',
+                        text: 'Bạn không được quyền xóa!',
+                        showConfirmButton: false,
+                        timer: 1000
+                    });
+                }
+            });
+
+        }
+    })
+});
