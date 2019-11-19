353c11df85114709065f6fd9b2dcdba928ef3256
minhlc98 <=>
2019-11-19 09:25:14 +0700

    completed (50%) Show list of publishers Delete Publisher

--- BookStore/Areas/Admin/Controllers/PublisherController.cs
+++ BookStore/Areas/Admin/Controllers/PublisherController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using BookStore.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Areas.Admin.Controllers
@@ -20,7 +22,44 @@ namespace BookStore.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Title = "Publishers";
+            var publishers = _ctx.Publishers.ToList();
+            return View(publishers);
+        }
+        public int Delete(int id)
+        {
+            var publisher = _ctx.Publishers.SingleOrDefault(p => p.PublisherId == id);
+            if (publisher != null)
+            {
+                _ctx.Publishers.Remove(publisher);
+                _ctx.SaveChanges();
+                return 1;
+            }
+            return 0;
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            var pub = _ctx.Publishers.SingleOrDefault(p => p.PublisherId == id);
+            return PartialView("Edit",pub);
+        }
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile img)
+        {
+           
+                if (img != null)
+                {
+                    string fileName = $"{DateTime.Now.Ticks}{img.FileName}";
+                    string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","Admin" ,"Image", fileName);
+
+                    using (var f = new FileStream(fullPath, FileMode.Create))
+                    {
+                        img.CopyTo(f);
+                    }
+                    
+                }
+
+            return Content("Uploaded");
         }
     }
 }
---
+++ BookStore/wwwroot/Admin/js/Publisher.js
@@ -0,0 +1,51 @@
+﻿var table = $('#tablePublishers').DataTable();
+$('#tablePublishers tbody').on('click', '.btnDelete', function () {
+    var idItem = $(this).data("id");
+    var nameItem = $(this).data("name");
+    var item = $(this).parents('tr');
+    Swal.fire({
+        html: '<h2>Bạn có chắn xóa <b>' + nameItem + "</b>?</h2>",
+        text: "Bạn không thể hoàn tác!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Đúng, xóa.!'
+    }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                url: "/Admin/Publisher/Delete",
+                type: "POST",
+                data: {
+                    id: idItem
+                },
+                success: function (data) {
+                    if (data == 1) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Thành công!',
+                            text: 'Xóa thành công!',
+                            showConfirmButton: false,
+                            timer: 1000
+                        });
+                        table
+                            .row(item)
+                            .remove()
+                            .draw();
+                    }
+                    else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Lỗi',
+                            text: 'Không thành công!',
+                            showConfirmButton: false,
+                            timer: 1000
+                        });
+                    }
+                }
+            });
+
+        }
+    })
+
+});
