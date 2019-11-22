59b64b940402fd91b0fbfac932bd80f8d17fcd4e
Quoc Ky <dqky.hcmup@gmail.com>
2019-11-22 19:46:17 +0700

    Update Customer include : (Edit, Add, Detail) Add file MyTool Add function update avatar user

--- BookStore/Areas/Admin/Controllers/CustomerController.cs
+++ BookStore/Areas/Admin/Controllers/CustomerController.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BookStore.Helpers;
+﻿using BookStore.Helpers;
 using BookStore.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BookStore.Areas.Admin.Controllers
 {
@@ -33,20 +33,31 @@ namespace BookStore.Areas.Admin.Controllers
             Customer customer = new Customer();
             return View(customer);
         }
+
         [HttpPost]
-        public async Task<IActionResult> Add(Customer customer)
+        public async Task<IActionResult> Add(Customer customer, string NameImage)
         {
-            bool check = _context.Customer.Any(c => c.UserName == customer.UserName);
-            customer.CreatedDate = DateTime.Now;
-            if (!check)
+            if (ModelState.IsValid && customer != null)
             {
-                _context.Customer.Add(customer);
-                await _context.SaveChangesAsync();
+                bool check = _context.Customer.Any(c => c.UserName == customer.UserName);
+                if (!check)
+                {
+                    MoveImage(NameImage);
+                    customer.Image = NameImage;
+                    customer.CreatedDate = DateTime.Now;
+                    _context.Customer.Add(customer);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
             else
             {
                 return BadRequest();
             }
+            
             var model = await _context.Customer.AsNoTracking().ToListAsync();
             return View("Datatable", model);
         }
@@ -81,11 +92,26 @@ namespace BookStore.Areas.Admin.Controllers
             {
                 return BadRequest();
             }
+
             return View(customer);
         }
+
         [HttpPost]
-        public async Task<IActionResult> Edit(Customer customer)
+        public IActionResult UploadImage([FromForm]IFormFile file)
         {
+            if (file != null)
+            {
+                var name = MyTool.UploadHinh(file, "Tmp");
+                return Ok(new { name = name });
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Customer customer, string NameImage)
+        {
+            MoveImage(NameImage);
+
             var cus = await _context.Customer.AsNoTracking().SingleOrDefaultAsync(e => e.CustomerId == customer.CustomerId);
 
             if (customer.Password != cus.Password)
@@ -97,6 +123,7 @@ namespace BookStore.Areas.Admin.Controllers
             {
                 try
                 {
+                    customer.Image = NameImage;
                     _context.Update(customer);
                     await _context.SaveChangesAsync();
                 }
@@ -117,6 +144,33 @@ namespace BookStore.Areas.Admin.Controllers
             _context.Customer.Remove(customer);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customer.Any(c => c.CustomerId == id);
+        }
+
+        private void MoveImage(string NameImage)
+        {
+            /*----Start Move file from one folder to another folder*/
+            var sourcePath = "wwwroot/img/Tmp/" + NameImage;
+            var destinationPath = "wwwroot/img/Customer/" + NameImage;
+            if (System.IO.File.Exists(sourcePath))
+            {
+                System.IO.File.Move(sourcePath, destinationPath);
+            }
+            /*----End Move file from one folder to another folder*/
+
+            /*----Start Delete file from folder*/
+            var path = "wwwroot/img/Tmp/";
+            System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            /*----End Delete file from folder*/
         }
     }
 }
