8f3589ae2c09d0166353c6c078be44d2588528b5
Quoc Ky <dqky.hcmup@gmail.com>
2019-11-30 19:30:41 +0700

    Modified link site in OnePay, PayPal

--- BookStore/wwwroot/js/jsScriptByMySelf/Checkout.js
+++ BookStore/wwwroot/js/jsScriptByMySelf/Checkout.js
@@ -55,13 +55,13 @@ $("#Payment").click(function (e) {
         var order = $("#InfoOrder").serialize();
         var payMethod = $("input[name='PayMethod']:checked").val();
         if (payMethod == "Paypal") {
-            window.location = "https://localhost:44300/Paypal/Checkout?" + order;
+            window.location = "http://mybookstore.somee.com/Paypal/Checkout?" + order;
         }
         else if (payMethod == "OnePay") {
-        window.location = "https://localhost:44300/OnePay/OnePayPayment?" + order;
+            window.location = "http://mybookstore.somee.com/OnePay/OnePayPayment?" + order;
         }
         else if (payMethod == "DirectPay") {
-            window.location = "https://localhost:44300/Checkout/Invoice?" + order;
+            window.location = "http://mybookstore.somee.com/Checkout/Invoice?" + order;
         }
     }
 });
